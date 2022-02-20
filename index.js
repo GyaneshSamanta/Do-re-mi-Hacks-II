d0be09fbd248b764203695160197677324c8f814
pratik-chandrasekhar-pattanaik <chandrasekhar.pcp@gmail.com>
2022-02-20 17:06:27 +0530

    up

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
 app.use('/', require('./routes'));
-app.listen(port, function(err){
+app.listen(process.env.PORT || port, function(err){
     if(err){
         console.log(`error in running the server: ${err}`);
     }
